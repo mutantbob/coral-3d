ab50bfb7eea3d2782ec77d7c04a7c007e14e5c5a
Robert Forsman <git@thoth.purplefrog.com>
2013-05-02 21:23:36 -0400

    Finish the color-change logic. Fix a typo.

--- exp2.py
+++ exp2.py
@@ -18,9 +18,9 @@ def duplicateObject(scene, name, copyobj):
     ob_new.scale = copyobj.scale
     ob_new.location = copyobj.location
  
-    while 0<len(obj_new.data.materials.items()):
-        obj_new.data.materials.pop(0,1)
-
+    while 0<len(ob_new.data.materials.items()):
+        ob_new.data.materials.pop(0,1)
+        
     # Link new object to the given scene and select it
     scene.objects.link(ob_new)
     ob_new.select = True
@@ -29,14 +29,23 @@ def duplicateObject(scene, name, copyobj):
 
 def popUpMaterial( c, t0, t1, t2):
     rval = bpy.data.materials.new('fadeIn')
+    rval.use_transparency=1
     rval.diffuse_color=(1,1,1)
     rval.alpha=0
+    rval.emit=1
+    rval.diffuse_intensity=0
     rval.keyframe_insert(data_path='alpha', frame=t0)
     rval.alpha=1
     rval.keyframe_insert(data_path='alpha', frame=t1)
     rval.keyframe_insert(data_path='diffuse_color', frame=t1)
+    rval.keyframe_insert(data_path='diffuse_intensity', frame=t1)
+    rval.keyframe_insert(data_path='emit', frame=t1)
     rval.diffuse_color=c
+    rval.emit=0
+    rval.diffuse_intensity=0.8
     rval.keyframe_insert(data_path='diffuse_color', frame=t2)
+    rval.keyframe_insert(data_path='diffuse_intensity', frame=t2)
+    rval.keyframe_insert(data_path='emit', frame=t2)
     return rval
 
 
@@ -60,4 +69,16 @@ def fillLayer(x0,y0,z0, xCount,yCount, t0,dt):
             m1 = popUpMaterial( (0,0.8,0) , fr, fr+5, fr+10)
             o2.data.materials.append(m1)
 
-fillLayer(-20,0,0, 5,4, 5,3)
+
+def addLump(x,y,z, t):
+    o2 = duplicateObject(bpy.context.scene, "bacon", bpy.context.object)
+    o2.location = (x,y,z)
+    o2.hide_render=1
+    fr = t*10
+    o2.keyframe_insert(data_path="hide_render", frame=1)
+    o2.hide_render = 0
+    o2.keyframe_insert(data_path="hide_render", frame=fr)
+    m1 = popUpMaterial( (0,0.8,0) , fr, fr+7, fr+20)
+    o2.data.materials.append(m1)
+
+#fillLayer(-20,0,0, 5,4, 5,3)
